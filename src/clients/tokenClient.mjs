05ccd4268d7a8a22456efc494393f9e6b51eb7c8
Lee-Jin-Seop <jjjp9966@gmail.com>
2025-02-21 17:27:59 +0900

    feat(client): getTokens 함수 구현

--- src/clients/tokenClient.mjs
+++ src/clients/tokenClient.mjs
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const { CLIENT_ID, CLIENT_SECRET } = process.env;
+const regions = ["us", "eu", "kr", "tw"];
 
 const getAccessToken = async () => {
   if (!CLIENT_ID || !CLIENT_SECRET) {
@@ -31,3 +32,44 @@ const getAccessToken = async () => {
     throw new Error("Failed to get access token.");
   }
 };
+
+const getTokens = async () => {
+  try {
+    const accessToken = await getAccessToken();
+
+    const tokens = await Promise.all(
+      regions.map((region) => {
+        return getTokenForRegion(region, accessToken);
+      })
+    );
+
+    return tokens;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getTokenForRegion = async (region, accessToken) => {
+  try {
+    const response = await axios.get(
+      `https://${region}.api.blizzard.com/data/wow/token/?namespace=dynamic-${region}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    const { price, last_updated_timestamp } = response.data;
+
+    return {
+      region,
+      price: price / 10000,
+      lastUpdatedTimestamp: last_updated_timestamp,
+    };
+  } catch (error) {
+    throw new Error(`Failed to get token from ${region} region.`);
+  }
+};
+
+export { getTokens };
